fc22f3ec3b612fd8494184cdc1e8e61745e09852
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-05-14 12:46:42 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,58 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import { useState } from 'react';
+
+
+const App = ()=>{
+    let [inputvalue,setInputValue] = useState("");
+    function renderInput(e){
+        let numberValue = e.target;
+        setInputValue(inputvalue+numberValue.innerText);
+
+    }
+    function calculateInput(){
+        try{
+            let ans = eval(inputvalue);
+            setInputValue(ans);
+
+        }
+        catch(err){
+            setInputValue("Invalid Input");
+        }
+        
+        
+    }
+    function clearInput(){
+        setInputValue("");
+    }
+    return(
+        <div className='calc-div'>
+        <h1>Calculator</h1>
+        <input type='text' value={inputvalue} readOnly></input>
+        <div className="btn-div">
+        <button onClick={clearInput}>C</button>
+        <button onClick={renderInput}>/</button>
+        <button onClick={renderInput}>*</button>
+        <button onClick={renderInput}>-</button>
+        <button onClick={renderInput}>7</button>
+        <button onClick={renderInput}>8</button>
+        <button onClick={renderInput}>9</button>
+        <button onClick={renderInput}>+</button>
+        <button onClick={renderInput}>4</button>
+        <button onClick={renderInput}>5</button>
+        <button onClick={renderInput}>6</button>
+        <button onClick={renderInput}>1</button>
+        <button onClick={renderInput}>2</button>
+        <button onClick={renderInput}>3</button>
+        <button onClick={calculateInput}>=</button>
+        <button onClick={renderInput}>0</button>
+        <button onClick={renderInput}>.</button>
+
+        </div>
+        
+        </div>
+
+    )
+    
+}
+export default App;
 
